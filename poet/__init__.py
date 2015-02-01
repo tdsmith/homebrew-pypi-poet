ead2486b6554a0c7c0cff240f9d97c76eb5ba7da
Tim D. Smith <git@tim-smith.us>
2015-01-31 23:37:30 -0800

    setuptoolify

--- poet/__init__.py
+++ poet/__init__.py
@@ -1,3 +1,3 @@
 from .poet import *
 
-__version__ = "0.3"
+__version__ = "0.3.0"
---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+import poet
+
+setup(
+    name='homebrew-pypi-poet',
+    version=poet.__version__,
+    url='https://github.com/tdsmith/homebrew-pypi-poet',
+    license='MIT',
+    author='Tim D. Smith',
+    author_email='poet@tim-smith.us',
+    description='Writes Homebrew stanzas for pypi packages',
+    packages=['poet'],
+    platforms='any',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7'
+    ],
+    install_requires = ['jinja2', 'networkx', 'pip', 'tl.eggdeps'],
+    entry_points = {'console_scripts': ['poet=poet:main']}
+)
