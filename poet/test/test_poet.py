09b5c5a6b4a3e67ea4ddcdeb9dddc70152c7b0e0
Tim D. Smith <git@tim-smith.us>
2016-06-08 23:16:11 -0700

    Prettify

--- poet/test/test_poet.py
+++ poet/test/test_poet.py
@@ -3,18 +3,22 @@
 import subprocess
 import sys
 
+
 def poet(*args):
     return subprocess.check_output(
-            ["poet"] + list(args),
-            stderr=subprocess.STDOUT)
+        ["poet"] + list(args),
+        stderr=subprocess.STDOUT)
+
 
 def test_version():
     assert b"homebrew-pypi-poet" in poet("-V")
+
 
 def test_single():
     result = poet("-s", "nose", "six")
     assert b'resource "nose"' in result
     assert b'resource "six"' in result
+
 
 def test_formula():
     result = poet("-f", "pytest")
@@ -24,14 +28,17 @@ def test_formula():
     else:
         assert b'depends_on :python3' in result
 
+
 def test_case_sensitivity():
-    result = poet("-f", "FoBiS.py")
+    poet("-f", "FoBiS.py")
+
 
 def test_resources():
     result = poet("pytest")
     assert b'resource "py" do' in result
     result = poet("py.test")
     assert b'PackageNotInstalledWarning' in result
+
 
 def test_audit(tmpdir):
     home = tmpdir.chdir()
