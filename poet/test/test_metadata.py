9d2bb26acda70fdc271d92b623fd9fc3ad6123b4
Justin Rice <jsrice7391@gmail.com>
2022-05-20 01:09:37 -0400

    Tried to add some tests

--- poet/test/test_metadata.py
+++ poet/test/test_metadata.py
@@ -1,4 +1,13 @@
-from poet.poet import get_checksum_from_pip_source_file, get_download_url_from_pip_source_file, get_metadata_from_pip_source
+import pytest
+from unittest import mock
+
+from poet import get_checksum_from_pip_source_file
 
 
-def test_get_checksum_from_pip_source_file():
+
+def test_get_checksum_from_pip_source_file(mocker):
+    mocker.patch("poet.sha256.hexdigest(", return_value="sha256:1234567")
+    assert get_checksum_from_pip_source_file("/path/to/pytest.py") == "sha256:1234567"
+
+
+
--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setup(
     author='Tim D. Smith',
     author_email='poet@tim-smith.us',
     description='Writes Homebrew stanzas for pypi packages',
+    extras_require={"dev": ["pytest", "pytest-mock"]},
     packages=['poet'],
     platforms='any',
     classifiers=[
@@ -22,7 +23,7 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.6',
     ],
-    install_requires=['jinja2', 'setuptools'],
+    install_requires=['jinja2', 'setuptools', 'urlextract'],
     entry_points={'console_scripts': [
         'poet=poet:main',
         'poet_lint=poet.lint:main',
