f59d58e4a11bfc66f9535fad8d4057b7a74befec
Justin Rice <jsrice7391@gmail.com>
2022-05-23 12:32:37 -0400

    Added the package metadata refactor

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.6',
     ],
-    install_requires=['jinja2', 'setuptools', 'urlextract'],
+    install_requires=['jinja2', 'setuptools', 'urlextract', 'packaging'],
     entry_points={'console_scripts': [
         'poet=poet:main',
         'poet_lint=poet.lint:main',
